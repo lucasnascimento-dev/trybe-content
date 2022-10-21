e0aa2845c0bd41cdd090f4628435cf0f082b088f
Lucas Nascimento <lnascimento.ec@gmail.com>
2022-10-20 22:06:49 -0300

    testes nas constantes - fim de exercício

--- 01-fundamentos/04-introducao-a-javascript/dia-01-javascript-primeiros-passos/variaveis/script.js
+++ 01-fundamentos/04-introducao-a-javascript/dia-01-javascript-primeiros-passos/variaveis/script.js
@@ -5,3 +5,12 @@ let birthYear = 1991;
 console.log(myName);
 console.log(birthCity);
 console.log(birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+// birthCity = "Jaboatão";
+
+/* no comando acima, por se tratar de uma constante, não podemos
+ reatribuir um novo valor */
