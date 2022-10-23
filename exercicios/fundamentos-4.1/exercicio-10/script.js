3cf63ca35b821adc2b8244d745ed7027ae24dafc
Lucas Nascimento <lnascimento.ec@gmail.com>
2022-10-23 16:11:53 -0300

    exercicio 10

---
+++ exercicios/fundamentos-4.1/exercicio-10/script.js
@@ -0,0 +1,33 @@
+/*
+Utilize if/else para escrever um programa que se inicie com dois valores em duas 
+constantes diferentes: o custo de um produto e seu valor de venda. A partir dos 
+valores, calcule quanto de lucro (valor de venda, descontado o custo do produto) a 
+empresa terá ao vender mil desses produtos.
+
+    -Atente que, sobre o custo do produto, incide um imposto de 20%.
+
+    -Seu programa também deve emitir uma mensagem de erro e encerrar caso algum dos seus valores de entrada seja menor que zero.
+
+    -O lucro de um produto é o resultado da subtração do valor de venda pelo custo do mesmo, sendo que o imposto de 20% também faz parte do valor de custo.
+        -valorCustoTotal = valorCusto + impostoSobreOCusto;
+        -lucro = valorVenda - valorCustoTotal (lucro de um produto);
+
+*/
+
+let custoProduto = 100;
+let valorVenda = 200;
+let quantidaVenda = 1000;
+
+custoProduto *= 1.2; //inserção de imposto de 20%
+ 
+let lucro = quantidaVenda*(valorVenda-custoProduto);
+
+if (custoProduto < 0 || valorVenda < 0) {
+    console.log("ERRO - VALORES INVÁLIDOS")
+}
+else {
+    console.log("seu lucro será de R$ "+lucro+",00")
+}
+
+
+
