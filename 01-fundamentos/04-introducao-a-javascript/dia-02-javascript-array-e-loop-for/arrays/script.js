0127d58e189675bfbdd6f375102e3c6fb4503ed3
Lucas Nascimento <lnascimento.ec@gmail.com>
2022-10-23 19:38:04 -0300

    exercícios de fixação 2

--- 01-fundamentos/04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/arrays/script.js
+++ 01-fundamentos/04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/arrays/script.js
@@ -1,4 +1,16 @@
+//EXERCÍCIOS DE FIXAÇÀO:
+/*
+    Obtenha o valor “Serviços” do array menu:
+*/
+
 let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 let menuServices = menu[1];
 
 console.log(menuServices);
+//---------------------------------------------------------------------------
+
+//Procure o índice do valor “Portfólio” do array menu:
+
+let searchPortfolio = menu.indexOf("Portfólio")
+
+console.log(searchPortfolio)
