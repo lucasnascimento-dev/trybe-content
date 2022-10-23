aed9ba9b63a34aee7125c9c89872112cb4af3f57
Lucas Nascimento <lnascimento.ec@gmail.com>
2022-10-23 13:31:49 -0300

    exercicio 1 - soma

---
+++ exercicios/fundamentos-4.1/exercicio-01/script.js
@@ -0,0 +1,24 @@
+// Faça cinco programas, um para cada operação aritmética básica.
+// Seu programa deve ter duas constantes, a e b, definidas no
+// começo com os valores que serão operados. Faça programas para:
+
+//     Adição (a + b)
+//     Subtração (a - b)
+//     Multiplicação (a * b)
+//     Divisão (a / b)
+//     Módulo (a % b)
+
+//----------------------DEFINIÇÃO DE CONSTANTES----------------------
+let a =3;
+let b = 4;
+//----------------------DEFINIÇÃO DE CONSTANTES----------------------
+
+//-------------------------PROGRAMA 1 (SOMA)-------------------------
+adicao = a+b;
+console.log('a + b = ' + adicao)
+//-------------------------PROGRAMA 1 (SOMA)-------------------------
+
+//-------------------------PROGRAMA 2 (Subtração)-------------------------
+subtracao = a-b;
+console.log('a - b = ' + subtracao)
+//-------------------------PROGRAMA 2 (Subtração)-------------------------
