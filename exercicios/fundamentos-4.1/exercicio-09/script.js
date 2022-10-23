39ff6cc04feb3fbdbe797102159d4ac2cdd4a706
Lucas Nascimento <lnascimento.ec@gmail.com>
2022-10-23 15:49:35 -0300

    exercicio 9

---
+++ exercicios/fundamentos-4.1/exercicio-09/script.js
@@ -0,0 +1,19 @@
+/*
+Utilize if/else para fazer um programa que defina três números em constantes e retorne
+ true se pelo menos uma das três for ímpar. Caso contrário, ele deve retornar false.
+
+    Bonus: use somente um if.
+
+*/
+
+let a = 3;
+let b = 6;
+let c = 24;
+
+let trueOrFalse = false;
+
+if (a % 2 != 0 || b % 2 != 0 || c % 2 != 0) {
+    trueOrFalse = true;
+}
+
+console.log(trueOrFalse);
