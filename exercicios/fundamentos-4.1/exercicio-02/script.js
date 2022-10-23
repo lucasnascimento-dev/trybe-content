079bd615137f4695d74b0634d63b31d600a63ae6
Lucas Nascimento <lnascimento.ec@gmail.com>
2022-10-23 13:59:28 -0300

    exercicio 4

--- exercicios/fundamentos-4.1/exercicio-02/script.js
+++ exercicios/fundamentos-4.1/exercicio-02/script.js
@@ -14,23 +14,3 @@ else {
     console.log('o maior número é: ' + b)
 }
 
-/*
-Utilize if/else para fazer um programa que retorne o maior de três
-números. Defina, no começo do programa, três constantes com os
-valores que serão comparados. 
-*/
-
-let c = 13;
-let d = 21;
-let e = 90;
-
-
-if (c > d && c >e) {
-    console.log('o maior número é: ' + c)
-}
-else if (d > c && d > e){
-    console.log('o maior número é: ' + d)
-}
-else {
-    console.log('o maior número é: ' + e)
-}
---
+++ exercicios/fundamentos-4.1/exercicio-04/script.js
@@ -0,0 +1,18 @@
+/*
+Utilize if/else para fazer um programa que, dado um valor recebido
+como parâmetro, retorne “positive” se esse valor for positivo,
+“negative” se for negativo, e caso não seja nem positivo e nem
+negativo retorne “zero”.
+*/
+
+let a = 10;
+
+if (a < 0) {
+    console.log('esse número é negativo')
+}
+else if (a > 0){
+    console.log('esse número é positivo')
+}
+else {
+    console.log('esse número é zero')
+}
