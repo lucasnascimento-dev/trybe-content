5efe8252adbe0afe118f7fd0fda0e2eba3a0e216
Lucas Nascimento <lnascimento.ec@gmail.com>
2022-10-24 12:19:43 -0300

    tarefa 9

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicios-agora-a-pratica/09-exercicios.js
@@ -0,0 +1,11 @@
+let numbers = [];
+
+for (index = 1 ; index < 26 ; index += 1) {
+    numbers.push(index)
+}
+
+console.log(numbers)
+
+for (index = 0 ; index < numbers.length ; index += 1) {
+    console.log(numbers[index]/2)
+}
