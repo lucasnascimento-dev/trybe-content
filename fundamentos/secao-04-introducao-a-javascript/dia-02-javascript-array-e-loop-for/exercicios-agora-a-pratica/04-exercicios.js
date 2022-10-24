a5d13c47fc06893d6e82dd29170a4a594c67a901
Lucas Nascimento <lnascimento.ec@gmail.com>
2022-10-24 11:34:11 -0300

    tarefa 4

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicios-agora-a-pratica/04-exercicios.js
@@ -0,0 +1,17 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for (index = 0 ; index < 10 ; index += 1) {
+    soma += numbers[index]
+}
+
+let media = soma/numbers.length
+
+console.log(media);
+
+if (media > 20) {
+    console.log("valor maior que 20");
+}
+else {
+    console.log("valor menor ou igual a 20");
+}
