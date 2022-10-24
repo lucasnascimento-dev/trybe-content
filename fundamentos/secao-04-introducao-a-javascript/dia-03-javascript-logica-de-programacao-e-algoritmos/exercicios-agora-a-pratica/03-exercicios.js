522136a353adce6568fc982c44774bf3021b8428
Lucas Nascimento <lnascimento.ec@gmail.com>
2022-10-24 18:50:59 -0300

    agora, a prática- exercicio 3

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios-agora-a-pratica/03-exercicios.js
@@ -0,0 +1,20 @@
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let smallWord = array[0];
+let bigWord = array[0];
+
+// for (let index = 0 ; index < array.length ; index += 1) {
+//     if (array[index].length < smallWord.length) {
+//         smallWord = array[index];
+//     }
+// }
+
+for (index = 0 ; index < array.length ; index += 1){
+    if (array[index].length > bigWord.length) {
+        bigWord = array[index];
+    }
+}
+
+
+
+console.log(smallWord)
+console.log(bigWord)
