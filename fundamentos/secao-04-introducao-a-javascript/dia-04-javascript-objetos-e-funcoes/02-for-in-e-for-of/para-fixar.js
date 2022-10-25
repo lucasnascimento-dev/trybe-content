b30ee46e6e510fae74f952795a2a6edc46425172
Lucas Nascimento <lnascimento.ec@gmail.com>
2022-10-25 18:20:58 -0300

    para fixar - for in e for of

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/02-for-in-e-for-of/para-fixar.js
@@ -0,0 +1,29 @@
+/*
+Usando o objeto abaixo, utilize for/in e imprima no console a 
+mensagem 'Olá xxxxx' para cada nome, substituindo o xxxxx pelo 
+nome em questão. 
+*/
+let names = {
+  person1: 'João',
+  person2: 'Maria',
+  person3: 'Jorge',
+};
+
+for (let chave in names) {
+  console.log('Olá ', names[chave])
+}
+
+/*
+Usando o objeto abaixo, utilize for/in e imprima um console.log 
+com as chaves e valores desse objeto.
+*/
+
+let car = {
+  model: 'A3 Sedan',
+  manufacturer: 'Audi',
+  year: 2020
+};
+
+for (let key in car ) {
+  console.log(key, ': ', car[key] )
+}
