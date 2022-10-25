aa5afb0762965a7a6908ef03704c6638f0093ac7
Lucas Nascimento <lnascimento.ec@gmail.com>
2022-10-25 15:15:32 -0300

    update

--- fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios-agora-a-pratica/03-exercicio.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios-agora-a-pratica/03-exercicio.js
@@ -1,4 +1,4 @@
-let array = ['java', 'javascript', 'python', 'html', 'css'];
+let array = ['java', 'javascript', 'python', 'html', 'css', 'paralelepípedo'];
 let smallWord = array[0];
 let bigWord = array[0];
 
@@ -6,15 +6,16 @@ for (let index = 0 ; index < array.length ; index += 1) {
     if (array[index].length < smallWord.length) {
         smallWord = array[index];
     }
-}
-
-for (index = 0 ; index < array.length ; index += 1){
     if (array[index].length > bigWord.length) {
         bigWord = array[index];
     }
 }
 
-
-
 console.log(smallWord)
 console.log(bigWord)
+
+// for (index = 0 ; index < array.length ; index += 1){
+//     if (array[index].length > bigWord.length) {
+//         bigWord = array[index];
+//     }
+// }
---
+++ fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios-agora-a-pratica/04-exercicio.js
@@ -0,0 +1,23 @@
+/*
+Um número primo é um número inteiro maior do que 1 que possui 
+somente dois divisores, ou seja, é divisível por 1 e por ele 
+mesmo. Sabendo disso, escreva um algoritmo que retorne o maior
+número primo entre 2 e 50.
+*/
+
+let number = 50;
+let biggestPrimeNumber = 0;
+
+for (let currentNumber = 2 ; currentNumber <= number ; currentNumber += 1){
+    let isPrime = true;
+    for (let currentDivisor = 2 ; currentDivisor < currentNumber ; currentDivisor += 1) {
+        if (currentNumber % currentDivisor === 0) {
+            isPrime = false;
+        }
+    }
+    if (isPrime){
+        biggestPrimeNumber = currentNumber;
+    }
+}
+
+console.log(biggestPrimeNumber)
