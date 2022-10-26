86da3c0b53ec4a6881794da850e54fd9a5594ac0
Lucas Nascimento <lnascimento.ec@gmail.com>
2022-10-26 13:47:45 -0300

    exercício - 6.1

--- fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicios/06-exercicio.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicios/06-exercicio.js
@@ -1,3 +1,10 @@
+/*
+Acesse as chaves nome, sobrenome e titulo, que está dentro da 
+chave livrosFavoritos, e faça um console.log no seguinte 
+formato: “O livro favorito de Julia Pessoa se chama 
+‘O Pior Dia de Todos’”.
+*/
+
 let leitor = {
     nome: 'Julia',
     sobrenome: 'Pessoa',
