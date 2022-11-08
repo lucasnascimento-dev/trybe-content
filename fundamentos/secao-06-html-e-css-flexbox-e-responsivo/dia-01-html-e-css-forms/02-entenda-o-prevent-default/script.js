f1c68fbe8f7c4680a9fe6ac07d8a03ee51fe2b13
Lucas Nascimento <lnascimento.ec@gmail.com>
2022-11-08 15:59:18 -0300

    02- exercício preventDefault()

--- fundamentos/secao-06-html-e-css-flexbox-e-responsivo/dia-01-html-e-css-forms/02-entenda-o-prevent-default/script.js
+++ fundamentos/secao-06-html-e-css-flexbox-e-responsivo/dia-01-html-e-css-forms/02-entenda-o-prevent-default/script.js
@@ -6,3 +6,13 @@ const HREF_LINK = document.querySelector("#href");
 HREF_LINK.addEventListener('click', (event) => {
     event.preventDefault();
 })
+
+INPUT_CHECKBOX.addEventListener('click', (event) => {
+    event.preventDefault();
+})
+
+INPUT_TEXT.addEventListener('keypress', (event) => {
+    if (event.key !== "a") {
+        event.preventDefault();
+    }
+})
