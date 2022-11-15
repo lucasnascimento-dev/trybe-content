09320b635a05f54da0dc06acfff27283342cbf17
Lucas Nascimento <lnascimento.ec@gmail.com>
2022-11-15 12:06:24 -0300

    exercício eventos dia 3 - parte 1

--- fundamentos/secao-05-javascipt:dom-eventos-web-storage/dia-03-javascript-eventos/02-exercicios-agora-a-pratica/script.js
+++ fundamentos/secao-05-javascipt:dom-eventos-web-storage/dia-03-javascript-eventos/02-exercicios-agora-a-pratica/script.js
@@ -14,4 +14,37 @@ const createDaysOfTheWeek = () => {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
-  
+  const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+  const createDaysOfTheMonth = () => {
+    const getDayList = document.querySelector('#days')
+    // const getDayList = document.getElementById('days')
+    console.log(getDayList)
+
+    for (let index = 0; index < decemberDaysList.length; index += 1) {
+      let day = decemberDaysList[index];
+      let dayItem = document.createElement('li')
+      
+      dayItem.innerHTML = day;
+      console.log(dayItem)
+      if (day === 24 || day === 31) {
+        //classe holiday 
+        dayItem.className = 'day holiday';
+        getDayList.appendChild(dayItem);
+      } else if (day === 4 || day === 11 || day ===18) {
+        //classe friday
+        dayItem.className = 'day friday';
+        getDayList.appendChild(dayItem);
+      } else if ( day === 25) {
+        // classe holiday e friday
+        dayItem.className = 'day friday holiday';
+        getDayList.appendChild(dayItem);
+      } else {
+        //classe day
+        dayItem.className ='day';
+        getDayList.appendChild(dayItem);
+      }
+    }
+  }
+
+  createDaysOfTheMonth();
