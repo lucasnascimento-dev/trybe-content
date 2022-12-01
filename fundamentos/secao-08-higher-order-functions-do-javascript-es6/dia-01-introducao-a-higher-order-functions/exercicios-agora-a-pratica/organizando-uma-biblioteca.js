5dc519fd3b146b5718a254e65c44c6104e75c2b2
Lucas Nascimento <lnascimento.ec@gmail.com>
2022-12-01 09:45:46 -0300

    resumo forEach, find, some e every

---
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/exercicios-agora-a-pratica/organizando-uma-biblioteca.js
@@ -0,0 +1,8 @@
+const pessoas = [
+    { nome: 'Joana', idade: 37 },
+    { nome: 'Ana', idade: 25 },
+    { nome: 'João', idade: 32 },
+    { nome: 'Aline', idade: 28 },
+  ];
+
+  pessoas.forEach((pessoa) => console.log(`Nome: ${pessoa.nome}`))
