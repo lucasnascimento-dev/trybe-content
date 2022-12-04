919f77c295118cd4e1d92b54d4ea862239215fcd
Lucas Nascimento <lnascimento.ec@gmail.com>
2022-12-04 14:54:52 -0300

    Exercícios - agora, a prática - 04

--- fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-3-higher-order-function-filter-e-reduce/exercicios-agora-a-pratica/exercise4.js
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-3-higher-order-function-filter-e-reduce/exercicios-agora-a-pratica/exercise4.js
@@ -61,18 +61,25 @@ const books = [
     },
   ];
   
-  const expectedResult = {
-    author: {
-      birthYear: 1948,
-      name: 'George R. R. Martin',
-    },
-    genre: 'Fantasia',
-    id: 1,
-    name: 'As Crônicas de Gelo e Fogo',
-    releaseYear: 1991,
-  };
-  
-  const getNamedBook = () => {
-    return books.find((book) => book.name.length === 26);
-  }
-  console.log(getNamedBook())
+  const expectedResult = [
+    'Frank Herbert',
+    'George R. R. Martin',
+    'Isaac Asimov',
+    'J. R. R. Tolkien',
+  ];
+
+const fantasyOrScienceFictionAuthors = () => {
+    const wantedGenres = ['Fantasia', 'Ficção Científica'];
+    return books
+    .filter((book) => wantedGenres.includes(book.genre))
+    .map((book) => book.author.name)
+    .sort()
+}
+
+console.log(fantasyOrScienceFictionAuthors())
+
+// .filter((generos)
+// if (generos.genre === 'Ficção Científica' || 
+// books.genre === 'Fantasia') {
+//     return generos
+// }
--- fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-3-higher-order-function-filter-e-reduce/exercicios-agora-a-pratica/exercise5.js
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-3-higher-order-function-filter-e-reduce/exercicios-agora-a-pratica/exercise5.js
@@ -61,18 +61,4 @@ const books = [
     },
   ];
   
-  const expectedResult = {
-    author: {
-      birthYear: 1948,
-      name: 'George R. R. Martin',
-    },
-    genre: 'Fantasia',
-    id: 1,
-    name: 'As Crônicas de Gelo e Fogo',
-    releaseYear: 1991,
-  };
-  
-  const getNamedBook = () => {
-    return books.find((book) => book.name.length === 26);
-  }
-  console.log(getNamedBook())
+  // Adicione o código do exercício aqui:
