65dadd50c6ffb548946493d01a82c8a76d247767
Lucas Nascimento <lnascimento.ec@gmail.com>
2022-12-05 22:43:07 -0300

    O desafio prático - 04

---
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-3-higher-order-function-filter-e-reduce/exercicios-agora-a-pratica/o-desafio-pratico/exercise4.js
@@ -0,0 +1,17 @@
+const names = [
+    'Aanemarie', 'Adervandes', 'Akifusa',
+    'Abegildo', 'Adicellia', 'Aladonata',
+    'Abeladerco', 'Adieidy', 'Alarucha',
+  ];
+  
+  const expectedResult = 20;
+  
+  const lettersArray = names.join('').toLocaleLowerCase().split('')
+  console.log(lettersArray)
+
+  const countA = () => {
+    return lettersArray.reduce((acc, curr) => 
+    curr === 'a' ? acc += 1 : acc, 0)
+  }
+
+  console.log(countA())
