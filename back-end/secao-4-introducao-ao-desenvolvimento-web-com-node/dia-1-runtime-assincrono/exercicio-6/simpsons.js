68404527aa08f253c7f0ef051c301c5a40033b9b
lucasnascimento-dev <lnascimento.ec@gmail.com>
2023-05-11 20:16:48 -0300

    seção 4 - dia 1 - exercício 6E

--- back-end/secao-4-introducao-ao-desenvolvimento-web-com-node/dia-1-runtime-assincrono/exercicio-6/simpsons.js
+++ back-end/secao-4-introducao-ao-desenvolvimento-web-com-node/dia-1-runtime-assincrono/exercicio-6/simpsons.js
@@ -35,14 +35,34 @@ async function CreateFile() {
   idToBeRemoved = [1, 2, 3, 4]
   const characterChosen = simpsons
     .filter((element) => idToBeRemoved.includes(Number(element.id)));
-  await fs.writeFile('./simpsonFamily.json', JSON.stringify(characterChosen));
+  await fs.writeFile('./simpsonsFamily.json', JSON.stringify(characterChosen));
+}
+
+async function addNelson() {
+  const fileContent = await fs.readFile('./simpsonsFamily.json', 'utf-8')
+  const simpsonsFamily = JSON.parse(fileContent)
+
+  simpsonsFamily.push({id: "8", name: 'Nelson Muntz'})
+  await fs.writeFile('./simpsonsFamily.json', JSON.stringify(simpsonsFamily));
+}
+
+async function replaceNelson() {
+  const fileContent = await fs.readFile('./simpsonsFamily.json', 'utf-8')
+  const simpsonsFamily = JSON.parse(fileContent)
+  
+  simpsonsFamily.pop()
+
+  simpsonsFamily.push({id: "5", name: 'Maggie Simpson'})
+  await fs.writeFile('./simpsonsFamily.json', JSON.stringify(simpsonsFamily));
 }
 
 main = () => {
   //readAll()
   //getSimpsomId(2).then((simpson) => console.log(simpson))
   //removeCharacter(10, 6)
-  CreateFile()
+  // CreateFile()
+  // addNelson();
+  replaceNelson();
 }
 
 main();
