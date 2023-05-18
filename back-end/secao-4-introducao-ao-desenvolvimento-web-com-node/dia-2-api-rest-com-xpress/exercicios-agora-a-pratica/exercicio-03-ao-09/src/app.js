c1e2bb40f3c42062b0bad4398d0ec8a8a2568cf9
lucasnascimento-dev <lnascimento.ec@gmail.com>
2023-05-18 17:03:40 -0300

    exercício de API REST Express corrigido

--- back-end/secao-4-introducao-ao-desenvolvimento-web-com-node/dia-2-api-rest-com-xpress/exercicios-agora-a-pratica/exercicio-03-ao-09/src/app.js
+++ back-end/secao-4-introducao-ao-desenvolvimento-web-com-node/dia-2-api-rest-com-xpress/exercicios-agora-a-pratica/exercicio-03-ao-09/src/app.js
@@ -13,82 +13,98 @@ const readFile = async () => {
     const data = await fs.readFile(moviesPath);
     return JSON.parse(data);
   } catch (error) {
-    console.log(`O arquivo não pôde ser lido: ${error}`)
+    console.log(`O arquivo não pôde ser lido: ${error}`);
   }
 };
 
-app.get('/movies/:id', async (req, res) => {
+app.get("/movies/:id", async (req, res) => {
   try {
     const movies = await readFile();
     const movie = movies.find((movie) => movie.id === Number(req.params.id));
-    return res.status(200).json(movie)
+    return res.status(200).json(movie);
   } catch (error) {
-    return res.status(500).send({ message: error.message })
+    return res.status(500).send({ message: error.message });
   }
 });
 
-app.get('/movies', async (req, res) => {
+app.get("/movies", async (req, res) => {
   try {
     const movies = await readFile();
     const movie = movies.map((movie) => movie);
-    return res.status(200).json(movie)
+    return res.status(200).json(movie);
   } catch (error) {
-    return res.status(500).send({ message: error.message })
+    return res.status(500).send({ message: error.message });
   }
 });
 
-app.post('/movies', async (req, res) => {
+app.post("/movies", async (req, res) => {
   try {
     const movies = await readFile();
     const { movie, price } = req.body;
 
     const newMovie = {
-      id: movies[movies.length -1].id + 1,
+      id: movies[movies.length - 1].id + 1,
       movie,
       price,
-    }
+    };
     const allMovies = JSON.stringify([...movies, newMovie]);
     await fs.writeFile(moviesPath, allMovies);
     res.status(201).json(newMovie);
-
   } catch (error) {
-    return res.status(500).send({ message: error.message })
+    return res.status(500).send({ message: error.message });
   }
 });
 
-app.put('/movies/:id', async (req, res) => {
+app.put("/movies/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    const { movie, price} = req.body;
+    const { movie, price } = req.body;
     const movies = await readFile();
 
     const index = movies.findIndex((element) => element.id === Number(id));
     movies[index] = { id: Number(id), movie, price };
-    
-    const updatedMovies = JSON.stringify(movies, null, 2)
-    await fs.writeFile(moviesPath, updatedMovies)
+
+    const updatedMovies = JSON.stringify(movies, null, 2);
+    await fs.writeFile(moviesPath, updatedMovies);
 
     res.status(200).json(movies[index]);
-
   } catch (error) {
-    return res.status(500).send({ message: error.message })
+    return res.status(500).send({ message: error.message });
   }
 });
 
-app.delete('movies/:id', async (req, res) => {
+app.delete("movies/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const movies = await readFile();
-  
-    const filteredMovies = movies.filter((element) => element.id !== Number(id));
-    
+
+    const filteredMovies = movies.filter(
+      (element) => element.id !== Number(id)
+    );
+
     const updatedMovies = JSON.stringify(filteredMovies, null, 2);
     await fs.writeFile(moviesPath, updatedMovies);
-  
+
     res.status(204).end();
-  
   } catch (error) {
-    return res.status(500).send({ message: error.message })
+    return res.status(500).send({ message: error.message });
+  }
+});
+
+app.get("/movies/search", async (req, res) => {
+  try {
+    const { q } = req.query;
+    const movies = await readFile();
+
+    if (q) {
+      const filteredMovies = movies.filter((element) => element.movie.includes(q));
+      return res.status(200).json(filteredMovies);
+    }
+    
+    return res.status(200).end
+
+  } catch (err) {
+    res.status(500).send({ message: err.message });
   }
 });
 
