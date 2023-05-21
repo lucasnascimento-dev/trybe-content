33a2efeaf8fe14b76f5905c5d92ab014e831ae15
lucasnascimento-dev <lnascimento.ec@gmail.com>
2023-05-21 19:34:10 -0300

    atualizando diretórios

--- back-end/secao-4-introducao-ao-desenvolvimento-web-com-node/dia-4-express-e-middlewares/exercicio-01/src/app.js
+++ back-end/secao-4-introducao-ao-desenvolvimento-web-com-node/dia-4-express-e-middlewares/exercicio-01/src/app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+
+const auth = require("./middlewares/auth");
 const validateName = require("./middlewares/validateName");
 const validatePrice = require("./middlewares/validatePrice");
 const validateDescription = require("./middlewares/validateDescription");
@@ -6,12 +8,15 @@ const validatedAt = require("./middlewares/validatedAt");
 const validateRating = require("./middlewares/validateRating");
 const validateDifficulty = require("./middlewares/validateDifficulty");
 
+const generateToken = require("./utils/generateToken");
+
 const app = express();
 
 app.use(express.json());
 
 app.post(
   "/activities",
+  auth,
   validateName,
   validatePrice,
   validateDescription,
@@ -22,5 +27,17 @@ app.post(
     res.status(201).json({ message: "Atividade cadastrada com sucesso!" });
   }
 );
+
+app.post("/signup", (req, res) => {
+  const { email, password, firstName, phone } = req.body;
+
+  if ([email, password, firstName, phone].includes(undefined)) {
+    return res.status(401).json({ message: "Campos ausentes!" });
+  }
+
+  const token = generateToken();
+
+  return res.status(200).json({ token });
+});
 
 module.exports = app;
