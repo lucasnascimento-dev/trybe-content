ca0c6f33575c18f39ea9eb12118d782b73ebf184
lucasnascimento-dev <lnascimento.ec@gmail.com>
2023-05-21 18:14:11 -0300

    Exercícios, agora, a prática - exercício 1.5

--- back-end/secao-4-introducao-ao-desenvolvimento-web-com-node/dia-4-express-e-middlewares/exercicio-01/src/app.js
+++ back-end/secao-4-introducao-ao-desenvolvimento-web-com-node/dia-4-express-e-middlewares/exercicio-01/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const validateName = require('./middlewares/validateName')
+const validateName = require('./middlewares/validateName');
+const validatePrice = require("./middlewares/validatePrice");
 
 const app = express();
 
@@ -7,7 +8,7 @@ app.use(express.json());
 
 
 
-app.post("/activities", validateName, (req, res) => {
+app.post("/activities", validateName, validatePrice, (req, res) => {
   res.status(201).json({ message: 'Atividade cadastrada com sucesso!' });
 });
 
