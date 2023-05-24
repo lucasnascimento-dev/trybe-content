0178b94a171983d8a89481ddf8c1b91b57cfaa6d
lucasnascimento-dev <lnascimento.ec@gmail.com>
2023-05-24 16:34:22 -0300

    adicionando o middleware para as rotas /people já dentro de um diretório de rotas

--- back-end/secao-4-introducao-ao-desenvolvimento-web-com-node/dia-5-express-e-mysql/trybecash-api/src/app.js
+++ back-end/secao-4-introducao-ao-desenvolvimento-web-com-node/dia-5-express-e-mysql/trybecash-api/src/app.js
@@ -1,7 +1,13 @@
 const express = require('express');
-
+const peopleRoutes = require('./routes/peopleRoutes')
 const app = express();
 
 app.use(express.json());
+app.use('/people', peopleRoutes);
+
+app.post('/people', (req, res) => {
+  const person = req.body;
+  res.status(201).json(person);
+})
 
 module.exports = app;
