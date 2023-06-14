c41bcff53eb179f0a5860188a8f6549b73f971a4
lucasnascimento-dev <lnascimento.ec@gmail.com>
2023-06-14 10:51:27 -0300

    exercícios - bônus

--- back-end/secao-3-funcoes-sql-joins-e-normalizacao/dia-2-descomplicando-joins/exercicios_agora_a_pratica/exercise.sql
+++ back-end/secao-3-funcoes-sql-joins-e-normalizacao/dia-2-descomplicando-joins/exercicios_agora_a_pratica/exercise.sql
@@ -56,3 +56,18 @@ RIGHT JOIN pixar.movies AS m
 	ON m.theater_id = t.id
 ORDER BY t.name;
 #-----------------------------------------------------------------------
+
+#EXERCISE 6 ------------------------------------------------------------
+SELECT
+	m.id,
+	m.title,
+  m.director,
+  m.year,
+  m.length_minutes,
+  m.theater_id
+FROM pixar.movies AS m
+INNER JOIN pixar.box_office AS b
+	ON b.movie_id = m.id
+WHERE b.rating > 8
+	AND m.theater_id IS NOT NULL
+#-----------------------------------------------------------------------
